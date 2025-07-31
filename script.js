8504a4f60f64cf789ecb9c42ca00a074ebd845f7
Mykhailo Rozhenko <mihailkfmn@gmail.com>
2025-07-31 07:40:51 +0300

    res

--- script.js
+++ script.js
@@ -1,41 +1,44 @@
-const formEL = document.querySelector('.feedback-form');
+document.addEventListener('DOMContentLoaded'),
+  () => {
+    const formEL = document.querySelector('.feedback-form');
 
-let formData = {
-  name: '',
-  message: '',
-};
+    let formData = {
+      name: '',
+      message: '',
+    };
 
-formEL.addEventListener('input', (e) => {
-  const name = e.currentTarget.elements.name.value;
-  const message = e.currentTarget.elements.message.value;
-  formData.name = name;
-  formData.message = message;
-  console.log(formData);
-  saveToLS('formData', formData);
-});
+    formEL.addEventListener('input', (e) => {
+      const name = e.currentTarget.elements.name.value;
+      const message = e.currentTarget.elements.message.value;
+      formData.name = name;
+      formData.message = message;
+      console.log(formData);
+      saveToLS('formData', formData);
+    });
 
-document.addEventListener('DOMContentLoaded', () => {
-  const lsData = getFromLS('formData');
+    document.addEventListener('DOMContentLoaded', () => {
+      const lsData = getFromLS('formData');
 
-  try {
-    formData = lsData;
-    formEL.elements.name.value = lsData.name;
-    formEL.elements.message.value = lsData.message;
-  } catch {}
-});
+      try {
+        formData = lsData;
+        formEL.elements.name.value = lsData.name;
+        formEL.elements.message.value = lsData.message;
+      } catch {}
+    });
 
-function saveToLS(key, value) {
-  const jsonData = JSON.stringify(value);
-  localStorage.setItem(key, jsonData);
-}
+    function saveToLS(key, value) {
+      const jsonData = JSON.stringify(value);
+      localStorage.setItem(key, jsonData);
+    }
 
-function getFromLS(key, defaultValue) {
-  const jsonData = localStorage.getItem(key);
-  try {
-    const data = JSON.parse(jsonData);
-    return data;
-  } catch {
-    console.log('ERROR PARSING');
-    return defaultValue || jsonData;
-  }
-}
+    function getFromLS(key, defaultValue) {
+      const jsonData = localStorage.getItem(key);
+      try {
+        const data = JSON.parse(jsonData);
+        return data;
+      } catch {
+        console.log('ERROR PARSING');
+        return defaultValue || jsonData;
+      }
+    }
+  };
